5c5053a5ea9532782e7591323b82660177acf368
ozw4 <zerowarurei@gmail.com>
2025-07-28 17:05:20 +0900

    Format translated modules

---
+++ src/osv/FaultCellGrid.py
@@ -0,0 +1,296 @@
+"""Python translation of FaultCellGrid."""
+
+from __future__ import annotations
+
+from typing import Iterable, List, Optional, Sequence, Union
+
+from math import inf
+
+
+# Assume FaultCell is defined in FaultCell.py in the same package
+from .FaultCell import FaultCell
+
+
+class FaultCellGrid:
+    """Fault cells in a 3D sampling grid."""
+
+    def __init__(
+        self,
+        arg1: Union[int, Sequence[FaultCell]],
+        arg2: Optional[int] = None,
+        arg3: Optional[int] = None,
+    ) -> None:
+        if isinstance(arg1, Sequence) and arg2 is None and arg3 is None:
+            self._init_from_cells(arg1)
+        elif isinstance(arg1, int) and arg2 is not None and arg3 is not None:
+            self._init_dimensions(arg1, arg2, arg3)
+        else:
+            raise ValueError("Invalid constructor arguments")
+
+    # ------------------------------------------------------------------
+    # Public getters
+    # ------------------------------------------------------------------
+    def get_n1(self) -> int:
+        return self._n1
+
+    def get_n2(self) -> int:
+        return self._n2
+
+    def get_n3(self) -> int:
+        return self._n3
+
+    def get_i1_min(self) -> int:
+        return self._j1
+
+    def get_i2_min(self) -> int:
+        return self._j2
+
+    def get_i3_min(self) -> int:
+        return self._j3
+
+    def get_i1_max(self) -> int:
+        return self._j1 + self._n1 - 1
+
+    def get_i2_max(self) -> int:
+        return self._j2 + self._n2 - 1
+
+    def get_i3_max(self) -> int:
+        return self._j3 + self._n3 - 1
+
+    # ------------------------------------------------------------------
+    # Cell accessors
+    # ------------------------------------------------------------------
+    def get(self, i1: int, i2: int, i3: int) -> Optional[FaultCell]:
+        i1 -= self._j1
+        i2 -= self._j2
+        i3 -= self._j3
+        if 0 <= i1 < self._n1 and 0 <= i2 < self._n2 and 0 <= i3 < self._n3:
+            return self._cells[i3][i2][i1]
+        return None
+
+    def set(self, obj: Union[FaultCell, Iterable[FaultCell]]) -> None:
+        if isinstance(obj, FaultCell):
+            i1 = obj.i1 - self._j1
+            i2 = obj.i2 - self._j2
+            i3 = obj.i3 - self._j3
+            self._cells[i3][i2][i1] = obj
+        else:
+            for c in obj:
+                self.set(c)
+
+    def set_cells_in_box(self, cell: FaultCell, d1: int, d2: int, d3: int) -> None:
+        c1, c2, c3 = cell.i1, cell.i2, cell.i3
+        b1 = max(c1 - d1, 0)
+        b2 = max(c2 - d2, 0)
+        b3 = max(c3 - d3, 0)
+        e1 = min(c1 + d1, self._n1 - 1)
+        e2 = min(c2 + d2, self._n2 - 1)
+        e3 = min(c3 + d3, self._n3 - 1)
+        for i3 in range(b3, e3 + 1):
+            for i2 in range(b2, e2 + 1):
+                for i1 in range(b1, e1 + 1):
+                    ci = self._cells[i3][i2][i1]
+                    if ci is not None:
+                        ci.used = True
+
+    def find_cell_in_box(
+        self,
+        c1: int,
+        c2: int,
+        c3: int,
+        d1: int,
+        d2: int,
+        d3: int,
+    ) -> Optional[FaultCell]:
+        b1 = max(c1 - d1, 0)
+        b2 = max(c2 - d2, 0)
+        b3 = max(c3 - d3, 0)
+        e1 = min(c1 + d1, self._n1 - 1)
+        e2 = min(c2 + d2, self._n2 - 1)
+        e3 = min(c3 + d3, self._n3 - 1)
+        dx = float("inf")
+        cell: Optional[FaultCell] = None
+        for i3 in range(b3, e3 + 1):
+            for i2 in range(b2, e2 + 1):
+                for i1 in range(b1, e1 + 1):
+                    ci = self._cells[i3][i2][i1]
+                    di = (c1 - i1) ** 2 + (c2 - i2) ** 2 + (c3 - i3) ** 2
+                    if ci is not None and ci.skin is None and di < dx:
+                        dx = di
+                        cell = ci
+        return cell
+
+    def find_cells_in_box(
+        self,
+        x1: float,
+        x2: float,
+        x3: float,
+        fp: float,
+        d1: int,
+        d2: int,
+        d3: int,
+    ) -> bool:
+        c1 = round(x1)
+        c2 = round(x2)
+        c3 = round(x3)
+        b1 = max(c1 - d1, 0)
+        b2 = max(c2 - d2, 0)
+        b3 = max(c3 - d3, 0)
+        e1 = min(c1 + d1, self._n1 - 1)
+        e2 = min(c2 + d2, self._n2 - 1)
+        e3 = min(c3 + d3, self._n3 - 1)
+        if fp > 180:
+            fp = 360 - fp
+        for i3 in range(b3, e3 + 1):
+            for i2 in range(b2, e2 + 1):
+                for i1 in range(b1, e1 + 1):
+                    ci = self._cells[i3][i2][i1]
+                    if ci is not None:
+                        fpi = ci.fp
+                        if fpi > 180:
+                            fpi = 360 - fpi
+                        dp = abs(fp - fpi)
+                        if dp < 40:
+                            return True
+        return False
+
+    def find_cells_in_box_cell(
+        self, cell: FaultCell, d1: int, d2: int, d3: int
+    ) -> bool:
+        c1, c2, c3 = cell.i1, cell.i2, cell.i3
+        fp = cell.fp
+        b1 = max(c1 - d1, 0)
+        b2 = max(c2 - d2, 0)
+        b3 = max(c3 - d3, 0)
+        e1 = min(c1 + d1, self._n1 - 1)
+        e2 = min(c2 + d2, self._n2 - 1)
+        e3 = min(c3 + d3, self._n3 - 1)
+        for i3 in range(b3, e3 + 1):
+            for i2 in range(b2, e2 + 1):
+                for i1 in range(b1, e1 + 1):
+                    ci = self._cells[i3][i2][i1]
+                    if ci is not None:
+                        fpi = ci.fp
+                        dp = abs(fp - fpi)
+                        dp = min(dp, 360 - dp)
+                        if dp < 40:
+                            return True
+        return False
+
+    def find_cell_above(self, cell: Optional[FaultCell]) -> Optional[FaultCell]:
+        if cell is None:
+            return None
+        if cell.ca is not None:
+            return cell.ca
+        return self._find_cell_above_below(True, cell)
+
+    def find_cell_below(self, cell: Optional[FaultCell]) -> Optional[FaultCell]:
+        if cell is None:
+            return None
+        if cell.cb is not None:
+            return cell.cb
+        return self._find_cell_above_below(False, cell)
+
+    def find_cell_left(self, cell: Optional[FaultCell]) -> Optional[FaultCell]:
+        if cell is None:
+            return None
+        if cell.cl is not None:
+            return cell.cl
+        return self._find_cell_left_right(True, cell)
+
+    def find_cell_right(self, cell: Optional[FaultCell]) -> Optional[FaultCell]:
+        if cell is None:
+            return None
+        if cell.cr is not None:
+            return cell.cr
+        return self._find_cell_left_right(False, cell)
+
+    # ------------------------------------------------------------------
+    # Private helpers
+    # ------------------------------------------------------------------
+    def _init_from_cells(self, cells: Sequence[FaultCell]) -> None:
+        i1min = min(c.i1 for c in cells)
+        i2min = min(c.i2 for c in cells)
+        i3min = min(c.i3 for c in cells)
+        i1max = max(c.i1 for c in cells)
+        i2max = max(c.i2 for c in cells)
+        i3max = max(c.i3 for c in cells)
+        self._j1 = i1min
+        self._j2 = i2min
+        self._j3 = i3min
+        self._n1 = 1 + i1max - i1min
+        self._n2 = 1 + i2max - i2min
+        self._n3 = 1 + i3max - i3min
+        self._cells = [
+            [[None for _ in range(self._n1)] for _ in range(self._n2)]
+            for _ in range(self._n3)
+        ]
+        for cell in cells:
+            self.set(cell)
+
+    def _init_dimensions(self, n1: int, n2: int, n3: int) -> None:
+        self._j1 = self._j2 = self._j3 = 0
+        self._n1 = n1
+        self._n2 = n2
+        self._n3 = n3
+        self._cells = [
+            [[None for _ in range(n1)] for _ in range(n2)] for _ in range(n3)
+        ]
+
+    def _find_cell_above_below(
+        self, above: bool, cell: FaultCell
+    ) -> Optional[FaultCell]:
+        i1, i2, i3 = cell.i1, cell.i2, cell.i3
+        x1, x2, x3 = cell.x1, cell.x2, cell.x3
+        u1, u2, u3 = cell.u1, cell.u2, cell.u3
+        k1 = 1
+        if above:
+            k1 = -k1
+            u1, u2, u3 = -u1, -u2, -u3
+        cmin: Optional[FaultCell] = None
+        dmin = inf
+        for k3 in range(-1, 2):
+            for k2 in range(-1, 2):
+                c = self.get(i1 + k1, i2 + k2, i3 + k3)
+                if c is not None:
+                    d1 = c.x1 - x1
+                    d2 = c.x2 - x2
+                    d3 = c.x3 - x3
+                    du = d1 * u1 + d2 * u2 + d3 * u3
+                    if du > 0.0:
+                        d1 -= du * u1
+                        d2 -= du * u2
+                        d3 -= du * u3
+                        d = d1 * d1 + d2 * d2 + d3 * d3
+                        if d < dmin:
+                            cmin = c
+                            dmin = d
+        return cmin
+
+    def _find_cell_left_right(self, left: bool, cell: FaultCell) -> Optional[FaultCell]:
+        i1, i2, i3 = cell.i1, cell.i2, cell.i3
+        x1, x2, x3 = cell.x1, cell.x2, cell.x3
+        v1, v2, v3 = cell.v1, cell.v2, cell.v3
+        if left:
+            v1, v2, v3 = -v1, -v2, -v3
+        cmin: Optional[FaultCell] = None
+        dmin = inf
+        for k3 in range(-5, 6):
+            for k2 in range(-5, 6):
+                if k2 == 0 and k3 == 0:
+                    continue
+                c = self.get(i1, i2 + k2, i3 + k3)
+                if c is not None:
+                    d1 = c.x1 - x1
+                    d2 = c.x2 - x2
+                    d3 = c.x3 - x3
+                    dv = d1 * v1 + d2 * v2 + d3 * v3
+                    if dv > 0.0:
+                        d1 -= dv * v1
+                        d2 -= dv * v2
+                        d3 -= dv * v3
+                        d = d1 * d1 + d2 * d2 + d3 * d3
+                        if d < dmin:
+                            cmin = c
+                            dmin = d
+        return cmin
---
+++ src/osv/FaultSkin.py
@@ -0,0 +1,406 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from math import acos, sqrt
+from typing import Iterable, Iterator, List, Optional, Sequence
+
+from .FaultCell import ColorMap, FaultCell
+
+
+@dataclass
+class TriangleGroup:
+    lhc: bool
+    xyz: List[float]
+    rgb: List[float]
+
+
+@dataclass
+class QuadGroup:
+    lhc: bool
+    xyz: List[float]
+    rgb: List[float]
+
+
+class FaultSkin:
+    """A linked list of fault cells used to analyze faults."""
+
+    def __init__(self) -> None:
+        self._seed: Optional[FaultCell] = None
+        self._cell_list: List[FaultCell] = []
+        self._cells_ab: Optional[List[List[FaultCell]]] = None
+        self._cells_lr: Optional[List[List[FaultCell]]] = None
+
+    # ------------------------------------------------------------------
+    # Basic utilities
+    # ------------------------------------------------------------------
+    def get_seed(self) -> Optional[FaultCell]:
+        return self._seed
+
+    def size(self) -> int:
+        return len(self._cell_list)
+
+    def get_cells(self) -> List[FaultCell]:
+        return list(self._cell_list)
+
+    # ------------------------------------------------------------------
+    # Static helpers
+    # ------------------------------------------------------------------
+    @staticmethod
+    def get_cells_all(skins: Sequence["FaultSkin"]) -> List[FaultCell]:
+        cells: List[FaultCell] = []
+        for skin in skins:
+            cells.extend(skin._cell_list)
+        return cells
+
+    @staticmethod
+    def get_fl(
+        n1: int, n2: int, n3: int, skins: Sequence["FaultSkin"]
+    ) -> List[List[List[float]]]:
+        fl = [[[0.0 for _ in range(n1)] for _ in range(n2)] for _ in range(n3)]
+        for skin in skins:
+            for cell in skin:
+                fl[cell.i3][cell.i2][cell.i1] = cell.fl
+        return fl
+
+    @staticmethod
+    def count_cells(skins: Sequence["FaultSkin"]) -> int:
+        return sum(skin.size() for skin in skins)
+
+    # ------------------------------------------------------------------
+    # Iteration
+    # ------------------------------------------------------------------
+    def __iter__(self) -> Iterator[FaultCell]:
+        return iter(self._cell_list)
+
+    # ------------------------------------------------------------------
+    # Cell arrays
+    # ------------------------------------------------------------------
+    def get_cells_ab(self) -> List[List[FaultCell]]:
+        if self._cells_ab is not None:
+            return self._cells_ab
+        cell_set = set()
+        cells_list: List[List[FaultCell]] = []
+        for cell in self._cell_list:
+            if cell not in cell_set:
+                c = cell
+                while c.ca is not None:
+                    c = c.ca
+                clist: List[FaultCell] = []
+                while c is not None:
+                    clist.append(c)
+                    cell_set.add(c)
+                    c = c.cb
+                cells_list.append(clist)
+        self._cells_ab = cells_list
+        return self._cells_ab
+
+    def get_cells_lr(self) -> List[List[FaultCell]]:
+        if self._cells_lr is not None:
+            return self._cells_lr
+        cell_set = set()
+        cells_list: List[List[FaultCell]] = []
+        for cell in self._cell_list:
+            if cell not in cell_set:
+                c = cell
+                while c.cl is not None and c.cl is not cell:
+                    c = c.cl
+                left = c
+                clist = [c]
+                cell_set.add(c)
+                c = c.cr
+                while c is not None and c is not left:
+                    clist.append(c)
+                    cell_set.add(c)
+                    c = c.cr
+                cells_list.append(clist)
+        self._cells_lr = cells_list
+        return self._cells_lr
+
+    # ------------------------------------------------------------------
+    # Processing utilities
+    # ------------------------------------------------------------------
+    def smooth_cell_normals(self, nsmooth: int) -> None:
+        for _ in range(nsmooth):
+            self._smooth_n(
+                lambda c: [c.w1, c.w2, c.w3],
+                lambda c, w: c.set_normal_vector(*w),
+            )
+
+    def get_cell_nearest_centroid(self) -> Optional[FaultCell]:
+        c1 = c2 = c3 = cs = 0.0
+        for cell in self._cell_list:
+            c1 += cell.fl * cell.x1
+            c2 += cell.fl * cell.x2
+            c3 += cell.fl * cell.x3
+            cs += cell.fl
+        if cs == 0.0:
+            return None
+        c1 /= cs
+        c2 /= cs
+        c3 /= cs
+        dmin = float("inf")
+        cmin = None
+        for cell in self._cell_list:
+            d = cell.distance_squared_to(c1, c2, c3)
+            if d < dmin:
+                dmin = d
+                cmin = cell
+        return cmin
+
+    def get_cell_xyz_uvw_rgb_for_likelihood(
+        self, size: float, cmap: ColorMap, lhc: bool
+    ) -> List[List[float]]:
+        return FaultCell.get_xyz_uvw_rgb_for_likelihood(
+            size, cmap, self.get_cells(), lhc
+        )
+
+    def get_cell_xyz_uvw_rgb_for_throw(
+        self, size: float, cmap: ColorMap, lhc: bool
+    ) -> List[List[float]]:
+        return FaultCell.get_xyz_uvw_rgb_for_throw(size, cmap, self.get_cells(), lhc)
+
+    def get_tri_mesh(self, cmap: ColorMap) -> TriangleGroup:
+        rgb: List[float] = []
+        xyz: List[float] = []
+        for cell in self._cell_list:
+            cr = cell.cr
+            cb = cell.cb
+            rb = cr.cb if cr else None
+            br = cb.cr if cb else None
+            if cr and rb:
+                xyz.extend([cell.x3, cell.x2, cell.x1])
+                rgb.append(cell.fl)
+                xyz.extend([rb.x3, rb.x2, rb.x1])
+                rgb.append(rb.fl)
+                xyz.extend([cr.x3, cr.x2, cr.x1])
+                rgb.append(cr.fl)
+            if cb and br:
+                xyz.extend([cell.x3, cell.x2, cell.x1])
+                rgb.append(cell.fl)
+                xyz.extend([cb.x3, cb.x2, cb.x1])
+                rgb.append(cb.fl)
+                xyz.extend([br.x3, br.x2, br.x1])
+                rgb.append(br.fl)
+        return TriangleGroup(True, xyz, cmap.get_rgb_floats(rgb))
+
+    def get_quad_mesh_strike(self, cmap: ColorMap) -> QuadGroup:
+        rgb: List[float] = []
+        xyz: List[float] = []
+        for cell in self._cell_list:
+            cb = cell.cb
+            cr = cell.cr
+            br = cb.cr if cb else None
+            fp = cell.fp
+            if cr and br and cr:
+                xyz.extend([cell.x3, cell.x2, cell.x1])
+                rgb.append(fp if fp <= 180 else 360 - fp)
+                xyz.extend([cb.x3, cb.x2, cb.x1])
+                fp = cb.fp
+                rgb.append(fp if fp <= 180 else 360 - fp)
+                xyz.extend([br.x3, br.x2, br.x1])
+                fp = br.fp
+                rgb.append(fp if fp <= 180 else 360 - fp)
+                xyz.extend([cr.x3, cr.x2, cr.x1])
+                fp = cr.fp
+                rgb.append(fp if fp <= 180 else 360 - fp)
+        return QuadGroup(True, xyz, cmap.get_rgb_floats(rgb))
+
+    def get_tri_mesh_strike(self, cmap: ColorMap) -> TriangleGroup:
+        rgb: List[float] = []
+        xyz: List[float] = []
+        for cell in self._cell_list:
+            cr = cell.cr
+            cb = cell.cb
+            rb = cr.cb if cr else None
+            br = cb.cr if cb else None
+            fp = cell.fp
+            if cr and rb:
+                xyz.extend([cell.x3, cell.x2, cell.x1])
+                rgb.append(fp if fp <= 180 else 360 - fp)
+                xyz.extend([rb.x3, rb.x2, rb.x1])
+                fp = rb.fp
+                rgb.append(fp if fp <= 180 else 360 - fp)
+                xyz.extend([cr.x3, cr.x2, cr.x1])
+                fp = cr.fp
+                rgb.append(fp if fp <= 180 else 360 - fp)
+            if cb and br:
+                xyz.extend([cell.x3, cell.x2, cell.x1])
+                fp = cell.fp
+                rgb.append(fp if fp <= 180 else 360 - fp)
+                xyz.extend([cb.x3, cb.x2, cb.x1])
+                fp = cb.fp
+                rgb.append(fp if fp <= 180 else 360 - fp)
+                xyz.extend([br.x3, br.x2, br.x1])
+                fp = br.fp
+                rgb.append(fp if fp <= 180 else 360 - fp)
+        return TriangleGroup(True, xyz, cmap.get_rgb_floats(rgb))
+
+    def update_strike(self) -> None:
+        for cell in self._cell_list:
+            cl = cell.cl
+            cr = cell.cr
+            fpl = fpr = scs = 0.0
+            if cl is not None:
+                d2 = cell.x2 - cl.x2
+                d3 = cell.x3 - cl.x3
+                ds = sqrt(d2 * d2 + d3 * d3)
+                if ds != 0:
+                    fpl = acos(d3 / ds)
+                    scs += 1.0
+            if cr is not None:
+                d2 = -cell.x2 + cr.x2
+                d3 = -cell.x3 + cr.x3
+                ds = sqrt(d2 * d2 + d3 * d3)
+                if ds != 0:
+                    fpr = acos(d3 / ds)
+                    scs += 1.0
+            if scs > 0.0:
+                cell.fp = (fpl + fpr) / scs
+
+    def get_x1max(self) -> float:
+        return max((c.x1 for c in self._cell_list), default=0.0)
+
+    def get_x2max(self) -> float:
+        return max((c.x2 for c in self._cell_list), default=0.0)
+
+    def get_x3max(self) -> float:
+        return max((c.x3 for c in self._cell_list), default=0.0)
+
+    def get_cell_links_xyz(self) -> List[List[float]]:
+        cells_ab = self.get_cells_ab()
+        cells_lr = self.get_cells_lr()
+        ns_ab = len(cells_ab)
+        ns_lr = len(cells_lr)
+        xyz_all: List[List[float]] = []
+        for iseg in range(ns_ab + ns_lr):
+            cells = cells_ab[iseg] if iseg < ns_ab else cells_lr[iseg - ns_ab]
+            ncell = len(cells)
+            np = ncell
+            if iseg >= ns_ab and cells[0].cl == cells[-1]:
+                np += 1
+            xyzi: List[float] = []
+            for ip in range(np):
+                cell = cells[ip % ncell]
+                xyzi.extend([cell.x3, cell.x2, cell.x1])
+            xyz_all.append(xyzi)
+        return xyz_all
+
+    # ------------------------------------------------------------------
+    # I/O utilities
+    # ------------------------------------------------------------------
+    @staticmethod
+    def read_from_file(file_name: str) -> "FaultSkin":
+        skin = FaultSkin()
+        with open(file_name, "rb") as f:
+            import struct
+
+            ncell = struct.unpack(">i", f.read(4))[0]
+            cell_list: List[FaultCell] = []
+            for _ in range(ncell):
+                x1, x2, x3, fl, fp, ft = struct.unpack(">ffffff", f.read(24))
+                cell = FaultCell(x1, x2, x3, fl, fp, ft)
+                cell.skin = skin
+                cell_list.append(cell)
+                cell.s1, cell.s2, cell.s3 = struct.unpack(">fff", f.read(12))
+            for cell in cell_list:
+                ida, idb, idl, idr = struct.unpack(">iiii", f.read(16))
+                if ida != -2147483648:
+                    cell.ca = cell_list[ida]
+                if idb != -2147483648:
+                    cell.cb = cell_list[idb]
+                if idl != -2147483648:
+                    cell.cl = cell_list[idl]
+                if idr != -2147483648:
+                    cell.cr = cell_list[idr]
+        skin._cell_list = cell_list
+        return skin
+
+    @staticmethod
+    def write_to_file(file_name: str, skin: "FaultSkin") -> None:
+        with open(file_name, "wb") as f:
+            import struct
+
+            f.write(struct.pack(">i", skin.size()))
+            for cell in skin:
+                f.write(
+                    struct.pack(
+                        ">ffffff", cell.x1, cell.x2, cell.x3, cell.fl, cell.fp, cell.ft
+                    )
+                )
+                f.write(struct.pack(">fff", cell.s1, cell.s2, cell.s3))
+            for cell in skin:
+                for nabor in (cell.ca, cell.cb, cell.cl, cell.cr):
+                    if nabor is not None:
+                        f.write(struct.pack(">i", nabor.id))
+                    else:
+                        f.write(struct.pack(">i", -2147483648))
+
+    @staticmethod
+    def read_from_file_slow(file_name: str) -> "FaultSkin":
+        import pickle
+
+        with open(file_name, "rb") as f:
+            return pickle.load(f)
+
+    @staticmethod
+    def write_to_file_slow(file_name: str, skin: "FaultSkin") -> None:
+        import pickle
+
+        with open(file_name, "wb") as f:
+            pickle.dump(skin, f)
+
+    # ------------------------------------------------------------------
+    # Package/internal methods
+    # ------------------------------------------------------------------
+    def add(self, cell: FaultCell) -> None:
+        assert cell.skin is None
+        cell.skin = self
+        if self._seed is None:
+            self._seed = cell
+        self._cell_list.append(cell)
+        self._cells_ab = None
+        self._cells_lr = None
+
+    def _smooth1(self, getter, setter) -> None:
+        ncell = self.size()
+        vals = [0.0] * ncell
+        cnts = [0.0] * ncell
+        cell_nabors: List[Optional[FaultCell]] = [None] * 4
+        for icell, cell in enumerate(self._cell_list):
+            val_cell = getter(cell)
+            cell_nabors[0] = cell.ca
+            cell_nabors[1] = cell.cb
+            cell_nabors[2] = cell.cl
+            cell_nabors[3] = cell.cr
+            for nabor in cell_nabors:
+                if nabor is not None:
+                    val_nabor = getter(nabor)
+                    vals[icell] += val_cell + val_nabor
+                    cnts[icell] += 2.0
+        for icell, cell in enumerate(self._cell_list):
+            cnti = cnts[icell]
+            vali = vals[icell] / (cnti if cnti > 0.0 else 1.0)
+            setter(cell, vali)
+
+    def _smooth_n(self, getter, setter) -> None:
+        ncell = self.size()
+        nval = len(getter(self._seed)) if self._seed else 0
+        vals = [[0.0] * nval for _ in range(ncell)]
+        cnts = [0.0] * ncell
+        cell_nabors: List[Optional[FaultCell]] = [None] * 4
+        for icell, cell in enumerate(self._cell_list):
+            vals_cell = getter(cell)
+            cell_nabors[0] = cell.ca
+            cell_nabors[1] = cell.cb
+            cell_nabors[2] = cell.cl
+            cell_nabors[3] = cell.cr
+            for nabor in cell_nabors:
+                if nabor is not None:
+                    vals_nabor = getter(nabor)
+                    for ival in range(nval):
+                        vals[icell][ival] += vals_cell[ival] + vals_nabor[ival]
+                    cnts[icell] += 2.0
+        for icell, cell in enumerate(self._cell_list):
+            cnti = cnts[icell]
+            scl = 1.0 / (cnti if cnti > 0.0 else 1.0)
+            setter(cell, [v * scl for v in vals[icell]])
